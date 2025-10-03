abf70b10d97724d6cddc221190a6444af9375375
Web Tech Dude <takenolove13579@gmail.com>
2025-10-03 11:33:46 +0000

    Did you make sure you removed everything related to firestore?

--- src/ai/dev.ts
+++
@@ -1,6 +0,0 @@
-import { config } from 'dotenv';
-config();
-
-import '@/ai/flows/generate-bio-content.ts';
-import '@/ai/flows/generate-project-description.ts';
-import '@/ai/flows/generate-resume-summary.ts';
