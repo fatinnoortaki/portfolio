b8003331ff3b01517c09a68b885ed135f543679f
Web Tech Dude <takenolove13579@gmail.com>
2025-10-03 13:08:58 +0000

    I meant, every time it loads and by only one time I meant everytime I hi

--- src/components/typewriter.tsx
+++ src/components/typewriter.tsx
@@ -1,38 +1,16 @@
 'use client';
 
 import { TypeAnimation } from 'react-type-animation';
-import { useState, useEffect } from 'react';
 
 type TypewriterProps = {
     text: string;
     className?: string;
 };
 
-const SESSION_STORAGE_KEY = 'typewriter_animation_done';
-
 export function Typewriter({ text, className }: TypewriterProps) {
-    const [hasAnimated, setHasAnimated] = useState(false);
-
-    useEffect(() => {
-        const animationDone = sessionStorage.getItem(SESSION_STORAGE_KEY);
-        if (animationDone) {
-            setHasAnimated(true);
-        }
-    }, []);
-
-    if (hasAnimated) {
-        return <span className={className}>{text}</span>;
-    }
-
     return (
         <TypeAnimation
-            sequence={[
-                text,
-                () => {
-                    sessionStorage.setItem(SESSION_STORAGE_KEY, 'true');
-                    setHasAnimated(true);
-                }
-            ]}
+            sequence={[text]}
             wrapper="span"
             cursor={true}
             repeat={0}
